ccb0e44575b15b3b3743f16de29445adce946ec7
MustMark <Superpeerawat.mm@gmail.com>
2024-10-11 23:19:59 +0700

    update navbar

--- web_app/frontend/src/components/Navbar.tsx
+++ web_app/frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import HamburgerIconClose from '../assets/Hamburger_icon_close.png'
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <nav className="fixed bg-et-yellow h-16 w-full flex min-[600px]:justify-center items-center justify-end z-50">
+        <nav className="fixed bg-et-yellow h-16 w-full flex min-[600px]:justify-center items-center justify-end z-50 ">
             <div className="hidden min-[600px]:flex text-white gap-12 font-semibold">
                 <a href="/">Home</a>
                 <a href="/mbti-test">MBTI Test</a>
@@ -26,7 +26,7 @@ function Navbar() {
                 </button>
             </div>
             {isOpen && (
-                <div className="absolute top-16 left-0 w-full bg-et-yellow min-[600px]:hidden">
+                <div className="absolute top-16 left-0 w-full bg-et-yellow min-[600px]:hidden bg-opacity-80">
                     <div className="flex flex-col text-white font-semibold">
                         <a href="/" className="py-2 px-4">Home</a>
                         <a href="/mbti-test" className="py-2 px-4">MBTI Test</a>
