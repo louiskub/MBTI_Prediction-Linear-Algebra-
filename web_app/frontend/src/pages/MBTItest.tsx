91ad03ab16d88ac1cbc915d7b00d7d24d025cc12
MustMark <Superpeerawat.mm@gmail.com>
2024-10-15 15:21:24 +0700

    update reset cookie button

--- web_app/frontend/src/pages/MBTItest.tsx
+++ web_app/frontend/src/pages/MBTItest.tsx
@@ -190,6 +190,29 @@ function MBTItest() {
         setSelectedValues(initialSelectedValues);
     }, []);
 
+    const resetAnswers = () => {
+        const newValues = Array(12).fill(null);
+        setSelectedValues(newValues);
+    
+        setName('');
+        setAge('');
+        setGender('');
+        setEducation('');
+        setInterest('');
+        setMBTI([]);
+
+        Cookies.remove('name');
+        Cookies.remove('age');
+        Cookies.remove('gender');
+        Cookies.remove('education');
+        Cookies.remove('interest');
+        Cookies.remove('MBTIResult');
+    
+        questions.forEach((_, index) => {
+            Cookies.remove(`question_${index}`);
+        });
+    };
+
     return (
         <div className='font-IBM bg-[#f7ebeb]'>
             <Navbar/>
@@ -197,6 +220,12 @@ function MBTItest() {
                 <h1 className='max-[400px]:text-4xl text-5xl sm:text-6xl font-bold p-16 text-et-brown text-center'>
                     แบบทดสอบ MBTI
                 </h1>
+                <button
+                    onClick={resetAnswers}
+                    className="rounded-md text-xl font-bold p-2 text-white bg-red-500 hover:bg-red-700 transition-transform transform hover:scale-105 hover:cursor-pointer shadow-sm mb-4"
+                >
+                    รีเซ็ตคำตอบ
+                </button>
                 <div className='bg-white w-5/6 sm:w-2/3 lg:w-[440px] max-w-[440px] rounded-md pt-4 pb-10 shadow-sm transition-transform hover:scale-105'>
                 <h1 className="text-2xl font-bold p-4 text-et-olive-brown text-center">ข้อมูลส่วนตัว</h1>
                     <div className="flex flex-col justify-center items-center p-2">
