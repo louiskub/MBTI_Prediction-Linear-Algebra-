e2db0fae3ee57a4bad7b53b253d9d7c043508cce
Anantha250 <129850658+Anantha250@users.noreply.github.com>
2024-10-17 09:41:13 +0700

    Beam commit

--- web_app/frontend/src/pages/Home.tsx
+++ web_app/frontend/src/pages/Home.tsx
@@ -77,7 +77,7 @@ const App: React.FC = () => {
         <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/watch?v=_Vdz-XK_JV8"
+            src="https://www.youtube.com/embed/_Vdz-XK_JV8"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
